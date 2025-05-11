4d423e73f50a18beb86f4f21329c53cab834deeb
Robin Gillespie <legowoodtrashcan@gmail.com>
2025-05-11 10:47:05 -0400

    added a bunch more functionality to the add-entry python script

--- utilities/add-entry.py
+++ utilities/add-entry.py
@@ -1,32 +1,86 @@
 import json
 import os
+import sys
 from datetime import datetime
 
 title = input("entry title: ")
 category = input("category (music, contraption, video, other): ").lower()
 while (category not in {"music", "contraption", "video", "other"}):
-    category=input("invalid category. try again: ")
+    category=input("\tinvalid category. try again: ")
 description = input("temp description: ")
 
 date=""
 if input("use today's date? (y/n) ").lower() == "y":
     date= datetime.today().strftime('%Y-%m-%d')        
 else:
-    date=input("release date: ")
-        
+    date=input("\trelease date: ")
+
+
+while True:
+    try:
+        numContents = int(input("how many tracks/items? (0 to exclude field): "))
+        break
+    except ValueError:
+        print("Please enter a valid number")
+if numContents == 0:
+    contents=[]
+else:
+    contents = []
+    for i in range(numContents):
+        contents.append(input("\titem #"+ str(i+1) + ": "))
+
+print("enter tags. enter 'done' to continue\nsome common tags are: [collab, single, ep, album, tutorial, shitpost, wip]")
+tag = ""
+tags = []
+while True:
+    tag = input("\tadd tag: ")
+    if (tag == "done"):
+        break
+    tags.append(input("\tadd tag: "))
+
+print("enter link titles (NOT URLS). enter 'done' to continue: ")
+link = ""
+links = {}
+while True:
+    link = input("\tenter link text: ")
+    if link == "done":
+        break
+    links[link] = input("\turl: ")
+
+includeMedia = True
+if input("include media? (y/n): ") != "y":
+    includeMedia = False
+
+
+print("-----------")
+print("\ttitle: "+title)
+print("\tcategory: "+category)
+print("\tdescription: "+description)
+print("\tdate: "+date)
+print("\tcontents: "+str(contents))
+print("\ttags: "+str(tags))
+print("\tlinks: "+str(links))
+if input("continue with entry creation? (y/n): ") != "y":
+    print("exiting...")
+    sys.exit()
+print("-----------")
 
 
 
 f = open('./content/_template/data.json')
 templatedata = json.load(f)
 
-
 templatedata["title"] = title
 templatedata["categories"][0] = category
 templatedata["dates"][date] = "published"
-
-
-print(templatedata)
+if contents == []:
+    del templatedata["contents"]
+else:
+    templatedata["contents"] = contents
+templatedata["tags"] = tags
+templatedata["links"] = links
+if not includeMedia:
+    del templatedata["media"]
 
 
 # MAKE FOLDER, POPULATE WITH BASIC FILES
@@ -57,6 +111,8 @@ with open('./content/index.json', 'r+') as indexfile:
     json.dump(indexdata, indexfile, indent=4)
     print("\tupdated index.json")
 
+print("done! make sure to go and check to verify that everything's correct, and to add anything that this script doesn't automate")
+print("add link urls, webpage, media, collaborators, etc")
 # indexdata = json.load(open("./content/index.json"))
 # indexdata = dict(sorted(indexdata.items(), key=lambda x: 
 #                     (''.join(c for c in x[1]['date'] if not c.isalpha())), 
--- utilities/stringtest.py
+++ utilities/stringtest.py
@@ -1,2 +1,11 @@
 g = "sjhfsdAUYGKUH"
 print(g.lower())
+
+
+try:
+    num = int(input("num: "))
+except ValueError:
+    print("Please enter a valid integer")
+    num = 0
+for i in range(num):
+    print(i)
