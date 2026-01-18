a785d76c1924730733d1ae633cec844f6accb668
Robin Gillespie <legowoodtrashcan@gmail.com>
2026-01-17 23:47:53 -0500

    complete rewrite of expert mode

--- content/script.js
+++ content/script.js
@@ -1,192 +1,256 @@
-fetch("./index.json").then(res => res.json()).then(index => {
-    // this goes through and creates all of the nessisary item divs in the correct order
-    // this has to be done first because all their contents will be loaded asynchronously
-    console.log(index);
-    Object.keys(index).forEach(async title => {
-        const path = title.toLowerCase().replaceAll(" ", "-")
-        let element = document.createElement("div");
-        element.className="item";
-        element.id = (title);
-        element.innerHTML = `
-            <span class="title" id="${title}-title">${title}</span><br><br>
-            <div class="links" id="${title}-links"></div>
-            <div class="dates" id="${title}-dates"></div>
-            <div class="description" id="${title}-description"></div>
-            <div class="category" id="${title}-category"></div>
-            <div class="tags" id="${title}-tags"></div>
-            <div class="contributors" id="${title}-contributors"></div>
-            <div class="contributor-links" id="${title}-contributor-links"></div>
-            <div class="contents" id="${title}-contents"></div>
-            <div class="audio" id="${title}-audio"></div>
-            <div class="media" id="${title}-media"></div>
-            <hr>
-            
-        `;
-        document.getElementById("content").appendChild(element);
+async function renderElement(title) {
+    let audiocount = 0;
 
-        // DESCRIPTION.TXT
-        {
-            try {
-                const response = await fetch('./'+path+"/description.txt");
-                if (response.ok) {
-                    const file = await response.text();
-                    if (file.length != 0) {
-                        document.getElementById(`${title}-description`).innerHTML += `
-                            <b>Description</b>:
-                            <div class="indent">${file}</div>
-                        `;
-                    } else {
-                        document.getElementById(`${title}-description`).innerHTML += `
-                            <b>Description</b>: <span class="error">description is empty</span>
-                        `;
-                    }
-                } else {
-                    document.getElementById(`${title}-description`).remove();
-                }
-            } catch (error) {}
-        }
+    const path = title.toLowerCase().replaceAll(" ", "-");
 
-        // DATA.JSON
-        {
-            try {
-                const response = await fetch('./'+path+"/data.json");
-                if (response.ok) {
-                    const data = await response.json();
-                    
-                    // TAGS
-                    {
-                        document.getElementById(`${title}-tags`).innerHTML += `
-                            <b>Tags</b>:
-                            ${data.tags.map(tag => `${tag}`).join(" | ")}
-                            <br>
-                        `;
-                    }
 
-                    // CATEGORY
-                    {
-                        document.getElementById(`${title}-category`).innerHTML += `
-                            <b>Category</b>:
-                            ${data.categories[0]}
-                            <br>
-                        `;
-                    }
+    let el_links = document.createElement('span');
+    el_links.id = `${title}-links`;
 
-                    // CONTRIBUTORS
-                    {
-                        // let element = document.createElement("span");
-                        let target = document.getElementById(`${title}-contributors`)
-                        target.innerHTML += "<b>Contributors</b>: "
-                        Object.keys(data.contributors).forEach(contributor => {
-                            // console.log(contributor);
-                            let button = document.createElement("button");
-                            button.innerHTML = contributor
-                            button.setAttribute("onclick", "fetch('../data/personData.json').then(res => res.json()).then(personData => {try{document.getElementById('"+title+"-contributor-links').innerHTML = `<b>"+contributor+":</b> "+data.contributors[contributor]+"<br><b>Links:</b> ${Object.keys(personData['"+contributor.toLowerCase()+"']).map(text => \"<a target='_blank' href='\"+personData['"+contributor.toLowerCase()+"'][text]+\"'>\"+text+\"</a>\").join(' | ')}` + `<br><button onclick=\"document.getElementById('"+title+"-contributor-links').innerHTML=null;\">hide</button>`;}catch{document.getElementById('"+title+"-contributor-links').innerHTML = `<b>"+contributor+"</b>: "+data.contributors[contributor]+"<br><b>Links:</b>  isn't in the database yet; no links` + `<br><button onclick=\"document.getElementById('"+title+"-contributor-links').innerHTML=null;\">hide</button>`;}})");
-                            target.appendChild(button)
-                            target.innerHTML += " | "
-                        });
-                        
-                    }
+    let el_dates = document.createElement('span');
+    el_dates.id = `${title}-dates`;
 
-                    // DATES
-                    {
-                        document.getElementById(`${title}-dates`).innerHTML += "<b>Date(s)</b>:<br>"
-                        for (const date of Object.keys(data["dates"])) {
-                            document.getElementById(`${title}-dates`).innerHTML += `
-                                <span class="indent">${date}: ${data["dates"][date]}</span><br>
-                            `
-                        }
-                    }
+    let el_description = document.createElement('span');
+    el_description.id = `${title}-description`;
 
-                    // CONTENTS
-                    {
-                        if (Object.keys(data).includes("contents")){
-                            document.getElementById(`${title}-contents`).innerHTML += `
-                                <b>Tracklist</b>:
-                                <ol>
-                                    ${data.contents.map((track) => `<li>${track}</li>`).join(``)}
-                                </ol>
-                            `;
-                        } else {
-                            document.getElementById(`${title}-contents`).remove();
-                        }
-                    }
+    let el_category = document.createElement('span');
+    el_category.id = `${title}-category`;
 
-                    // LINKS
-                    {
-                        if (Object.keys(data).includes("links")) {
-                            document.getElementById(`${title}-links`).innerHTML += `
-                            <b>Links</b>: ${Object.keys(data.links)
-                                .map(link => {
-                                    let url = data.links[link];
-                                    if (url.startsWith("./content/")) {
-                                        url = "."+url;
-                                    } 
-                                    return `<a href="${url}" target="_blank">${link}</a>
-                                `}).join(" | ")}
-                            `;
-                        } else {
-                            document.getElementById(`${title}-links`).remove();
-                        }
-                    }
+    let el_tags = document.createElement('span');
+    el_tags.id = `${title}-tags`;
 
-                    // AUDIO
-                    {
-                        if(Object.keys(data).includes("audio")) {
-                            // <audio controls src='${path}/audio/${filename}' type='audio/mp3'>
-                            document.getElementById(`${title}-audio`).innerHTML = `
-                                <b>Audio</b>:<br>
-                                ${data.audio.map((filename) => `
-                                    <audio controls src='${path}/audio/${filename}' type='audio/mp3'></audio> | <a href='${path}/audio/${filename}' download>${filename}</a>
-                                `).join("<br>")}
-                            `;
-                        // console.log(data.audio);
-                        }
-                    } 
+    let el_contributors = document.createElement('span');
+    el_contributors.id = `${title}-contributors`;
 
-                    //MEDIA
-                    {
-                        if(Object.keys(data).includes("media")) {
-                            document.getElementById(`${title}-media`).innerHTML += "<b>Media</b>:<br><br>";
-                            for (const filename of Object.keys(data.media)) {
-                                const imgPath = `${path}/${filename.replace(" ", "-")}`;
-                                document.getElementById(`${title}-media`).innerHTML += `
-                                
-                                <span class="media-item">
-                                    <a href="${imgPath}" target="_blank">
-                                        <img src="${imgPath}" width="250px"><br>
-                                    </a>
-                                    <span>${data.media[filename]}</span>
-                                </span>
-                                
-                                `;
-                            }
-                        }else {
-                            document.getElementById(`${title}-media`).remove();
-                        }
-                    }
+    let el_contents = document.createElement('span');
+    el_contents.id = `${title}-contents`;
 
-                    
-                } else {
-                    document.getElementById(`${title}-dates`).innerHTML = "<span class='error'>data file missing, or some other worse error</span><br>";
-                }
-            } catch (error) {}
-        }
+    let el_audio = document.createElement('span');
+    el_audio.id = `${title}-audio`;
     
-    });
-}).then( () => {
-    // scroll to entry in urlsearchparams
-    const params = Object.fromEntries(new URLSearchParams(window.location.search));
-    if (Object.keys(params).includes("entry")) {
-        const entry = params["entry"]
-        // console.log(entry);
-        document.getElementById(entry).scrollIntoView();
-        window.scrollBy(0, -25);
-        document.getElementById("home").remove();
+    let el_media = document.createElement('span');
+    el_media.id = `${title}-media`;
+
+
+    let element_fields = [
+        el_links,
+        el_dates,
+        el_description,
+        el_category,
+        el_tags,
+        el_contributors,
+        el_contents,
+        el_audio,
+        el_media
+    ]
+
+    let people;
+
+
+    // PEOPLE.JSON
+    {
+        try {
+            const response = await fetch("../data/personData.json");
+            if (response.ok) {
+                people = await response.json();
+            } 
+        } catch (error) {}
     }
-})
 
-// TODO: formalize abstraction to just return the rendered item widget instead of doing it in this spaghetti way
-function renderElement() {
+    // DESCRIPTION.TXT
+    {
+        try {
+            const response = await fetch('./'+path+"/description.txt");
+            if (response.ok) {
+                const file = await response.text();
+                if (file.length != 0) {
+                    el_description.innerHTML = `
+                        <b>Description</b>:
+                        <div class="indent">${file}</div>
+                    `;
+                } else {
+                    el_description.innerHTML = `
+                        <b>Description</b>: <span class="error">description is empty</span>
+                    `;
+                }
+            } 
+        } catch (error) {}
+    }
 
-    return
+
+
+    // DATA.JSON
+    {
+        try {
+            const response = await fetch('./'+path+"/data.json");
+            if (response.ok) {
+                const data = await response.json();
+                
+                // TAGS
+                el_tags.innerHTML = `
+                    <b>Tags</b>:
+                        ${data.tags.map(tag => `${tag}`).join(" | ")}
+                    <br>
+                `;
+
+                // CATEGORY
+                el_category.innerHTML = `
+                    <b>Category</b>:
+                    ${data.categories[0]}
+                    <br>
+                `;
+
+                // CONTRIBUTORS
+                el_contributors.innerHTML = `<b>Contributors</b>: <br>`
+                Object.keys(data.contributors).forEach(contributor => {
+                    let el_contributor = document.createElement("span");
+                    el_contributor.style = 'padding-left: 40px;'
+                    let contributor_lo = contributor.toLowerCase()
+
+                    el_contributor.innerHTML = `
+                        <b>${contributor}</b>:  ${data.contributors[contributor]} <br>
+                        
+                        <span style="padding-left: 80px;">
+                            ${Object.keys(people[contributor.toLowerCase()]).map(linkTitle => 
+                                `<a href="${people[contributor_lo][linkTitle]}">${linkTitle}</a> | `
+                            ).join("")}
+                        </span>
+                    `;
+
+                    el_contributors.appendChild(el_contributor)
+                    el_contributors.innerHTML += ' <br> '
+                });
+                    
+
+                // DATES
+                el_dates.innerHTML = `
+                    <b>Date(s)</b>:
+                    <br>
+                    ${Object.keys(data.dates).map(date => `
+                        <span class="indent">
+                            ${date}: ${data["dates"][date]}
+                        </span>
+                        <br>
+                    `).join('')}
+                `
+
+                // CONTENTS
+                if (Object.keys(data).includes("contents")){
+                    el_contents.innerHTML = `
+                        <b>Tracklist</b>:
+                        <ol>
+                            ${data.contents.map((track) => `<li>${track}</li>`).join(``)}
+                        </ol>
+                    `;
+                } 
+
+                // LINKS
+                if (Object.keys(data).includes("links")) {
+                    el_links.innerHTML = `
+                    <b>Links</b>: ${Object.keys(data.links)
+                        .map(link => {
+                            let url = data.links[link];
+                            if (url.startsWith("./content/")) {
+                                url = "."+url;
+                            } 
+                            return `<a href="${url}" target="_blank">${link}</a>
+                        `}).join(" | ")}
+                    <br>
+                    `;
+                }
+
+                // AUDIO
+                if(Object.keys(data).includes("audio")) {
+                    // <audio controls src='${path}/audio/${filename}' type='audio/mp3'></audio>
+                    el_audio.innerHTML = `
+                        <b>Audio</b>:
+                        <br>
+                        ${data.audio.map((filename) => `
+                            <audio controls src='${path}/audio/${filename}' type='audio/mpeg'></audio> | <a href='${path}/audio/${filename}' download>${filename}</a>
+                        `).join("<br>")}
+                        <br>
+                    `;
+                }
+
+
+                //MEDIA
+                if(Object.keys(data).includes("media")) {
+                    el_media.innerHTML = `
+                        <b>Media</b>:
+                        <br><br>
+                        ${Object.keys(data.media).map(filename => `
+                            <span class="media-item">
+                                <a href="${path}/${filename.replace(" ", "-")}" target="_blank">
+                                    <img src="${path}/${filename.replace(" ", "-")}" width="250px"><br>
+                                </a>
+                                <span>${data.media[filename]}</span>
+                            </span>
+                        `).join("")}
+                    `;
+                }
+
+                
+            } else {
+                document.getElementById(`${title}-dates`).innerHTML = "<span class='error'>data file missing, or some other worse error</span><br>";
+            }
+        } catch (error) {}
+    }
+
+    
+    let element = document.createElement("div");
+    element.className="item";
+    // element.id = (title);
+
+    element.innerHTML = `<span class="title" id="${title}-title">${title}</span><br><br>`
+    element_fields.forEach(field => {
+        // console.log(field)
+        element.appendChild(field);
+    });
+
+    return element;
 }
+
+const params = Object.fromEntries(new URLSearchParams(window.location.search));
+
+if (Object.keys(params).includes("entry")) {
+
+
+    const title = params["entry"]
+    renderElement(title).then(element => 
+        document.getElementById("content").appendChild(element)
+    );
+    
+    document.getElementById("home").remove();
+
+} else {
+    fetch("./index.json").then(res => res.json()).then(index => {
+        
+        Object.keys(index).forEach(title => {
+            
+            let item_container = document.createElement("div");
+            item_container.id=title;
+            document.getElementById("content").appendChild(item_container)
+
+        });
+
+
+        // this goes through and creates all of the nessisary item divs in the correct order
+        // this has to be done first because all their contents will be loaded asynchronously
+        console.log(index);
+        Object.keys(index).forEach(async title => {
+            renderElement(title).then(
+                element => {
+                    document.getElementById(title).appendChild(element);
+                    document.getElementById(title).innerHTML += "<br><hr><br>";
+                }
+            );
+        });
+    });
+
+      
+    
+    
+}
+
+
+
